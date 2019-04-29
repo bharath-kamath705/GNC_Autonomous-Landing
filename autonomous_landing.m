5c0f99cc5308e680129c9a936c5720bee95c36f6
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-04-29 11:25:13 -0500

    compute state matrix A

--- autonomous_landing.m
+++ autonomous_landing.m
@@ -1,4 +1,6 @@
 
+g=32.2 %acceleration due to gravity
+
 %aircraft data:altitude, moment of inertia etc
 data=xlsread('boeing747_data.xlsx')
 %case 1
@@ -9,6 +11,10 @@ W=data(4,1);
 I_x=data(5,1); I_y=data(6,1); I_z=data(7,1); I_zx=data(8,1);
 xi=data(9,1);
 C_d=data(10,1);
+m=W;
+%reference conditions
+theta0=0
+u0=V
 
 %Dimensional derivatives case 1 Mach 0.2 
 dd=xlsread('dimensional_derivatives_case1')
@@ -18,3 +24,12 @@ X_q=dd(3,1); Z_q=dd(3,2); M_q=dd(3,3);
 X_wdot=dd(4,1); Z_wdot=dd(4,2); M_wdot=dd(4,3);
 X_e=dd(5,1); Z_e=dd(5,2); M_e=dd(5,3);
 
+%case 1 state matrix A
+A1=zeros(4,4)
+A1(1,:)=[X_u/m, X_w/m, 0, -g*cos(theta0)]
+A1(2,:)=[Z_u, Z_w, Z_q+m*u0,-m*g*sin(theta0)]*inv(m-Z_wdot)
+A1(3,1)=inv(I_y)*(M_u+(M_wdot*Z_u*inv(m-Z_wdot)));
+A1(3,2)=inv(I_y)*(M_w+(M_wdot*Z_w*inv(m-Z_wdot)));
+A1(3,3)=inv(I_y)*(M_q+(M_wdot*(Z_q+m*u0)*inv(m-Z_wdot)));
+A1(3,4)=-M_wdot*m*g*sin(theta0)*inv(I_y*(m-Z_wdot));
+A1(4,:)=[0, 0, 1, 0]
