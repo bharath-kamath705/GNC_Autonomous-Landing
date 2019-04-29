7e3cafa4a8360b1d49057d9c09f4ccaffd8d77fe
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-04-29 10:47:08 -0500

    read and assign aircraft data from file

--- autonomous_landing.m
+++ autonomous_landing.m
@@ -1,6 +1,16 @@
 
+%aircraft data:altitude, moment of inertia etc
+data=xlsread('boeing747_data.xlsx')
+%case 1
+h=data(1,1);
+M_num=data(2,1);
+V=data(3,1);
+W=data(4,1);
+I_x=data(5,1); I_y=data(6,1); I_z=data(7,1); I_zx=data(8,1);
+xi=data(9,1);
+C_d=data(10,1);
 
-%Dimensional derivatives Mach 0.2 
+%Dimensional derivatives case 1 Mach 0.2 
 dd=xlsread('dimensional_derivatives_case1')
 X_u=dd(1,1); Z_u=dd(1,2); M_u=dd(1,3);
 X_w=dd(2,1); Z_w=dd(2,2); M_w=dd(2,3);
