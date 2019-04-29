99473d17ae5a2a23dd84e21e6cb52e3e9bd6e21f
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-04-29 10:34:43 -0500

    Update autonomous_landing.m

--- autonomous_landing.m
+++ autonomous_landing.m
@@ -1,3 +1,5 @@
+
+
 %Dimensional derivatives Mach 0.2 
 dd=xlsread('dimensional_derivatives_case1')
 X_u=dd(1,1); Z_u=dd(1,2); M_u=dd(1,3);
@@ -5,3 +7,4 @@ X_w=dd(2,1); Z_w=dd(2,2); M_w=dd(2,3);
 X_q=dd(3,1); Z_q=dd(3,2); M_q=dd(3,3);
 X_wdot=dd(4,1); Z_wdot=dd(4,2); M_wdot=dd(4,3);
 X_e=dd(5,1); Z_e=dd(5,2); M_e=dd(5,3);
+
