bf483d82cf3c246aac9e3bbd85df36b5f962c532
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-04-29 10:19:01 -0500

    variable rename for dimensional derivative

--- autonomous_landing.m
+++ autonomous_landing.m
@@ -1,7 +1,7 @@
 %Dimensional derivatives Mach 0.2 
-A=xlsread('dimensional_derivatives_case1')
-X_u=A(1,1); Z_u=A(1,2); M_u=A(1,3);
-X_w=A(2,1); Z_w=A(2,2); M_w=A(2,3);
-X_q=A(3,1); Z_q=A(3,2); M_q=A(3,3);
-X_wdot=A(4,1); Z_wdot=A(4,2); M_wdot=A(4,3);
-X_e=A(5,1); Z_e=A(5,2); M_e=A(5,3);
+dd=xlsread('dimensional_derivatives_case1')
+X_u=dd(1,1); Z_u=dd(1,2); M_u=dd(1,3);
+X_w=dd(2,1); Z_w=dd(2,2); M_w=dd(2,3);
+X_q=dd(3,1); Z_q=dd(3,2); M_q=dd(3,3);
+X_wdot=dd(4,1); Z_wdot=dd(4,2); M_wdot=dd(4,3);
+X_e=dd(5,1); Z_e=dd(5,2); M_e=dd(5,3);
