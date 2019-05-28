dc605f8f04fb9dfaab4e31a2811db31fecaad540
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-05-27 20:26:30 -0500

    ?

--- Untitled2.m
+++ Untitled2.m
@@ -1,4 +1,4 @@
-clc
+ clc
 clear all
 close all
 
@@ -25,32 +25,35 @@ u_ref=200 %data(3,1);
 
 %adaptive r(t)
 ws=10;
-C=[tan(gsa) -1 0 0; 0 1 0 0];
-A1_app=[A1 zeros(4,2); -C zeros(2,2)];
-B1_app =[B1; zeros(2,2)];
-D1=[zeros(4,1);-u_ref*tan(gsa); ws]; %-u_ref*tan(gsa)
+C=[tan(gsa) -1 0 0 0 0; 0 0 0 0 tan(gsa)  -1];
+A1_app=[A1 zeros(4,4);
+    -1 zeros(1,7);
+    0 1 zeros(1,6);
+    -C zeros(2,2)];
+B1_app=[B1; zeros(4,2)];
+D1=[zeros(4,1);-u_ref;0;-u_ref*tan(gsa); 0]; %-u_ref*tan(gsa)
 
 %assume arbitrary values for derivative of Z wrt thrust
 B1_app(2,2)=0.2;
 
 %check controllability
 disp('controlability matrix rank')
-P = [B1_app A1_app*B1_app A1_app^2*B1_app A1_app^3*B1_app A1_app^4*B1_app A1_app^5*B1_app ];
+P = [B1_app A1_app*B1_app A1_app^2*B1_app A1_app^3*B1_app A1_app^4*B1_app A1_app^5*B1_app A1_app^6*B1_app A1_app^7*B1_app];
 rank(P)
 
 
 %------------eigen structure----------------------
 %desired eigen values
-lambda1_d=[-2+j*0.5 -2-j*0.5 -0.05+ j*0 -2.9 -3.5 -4.5]
+lambda1_d=[-2+j*0.5 -2-j*0.5 -0.05+ j*0 -2.9 -3.5 -4.5 -5 -6]
 
 
 % Extracting desired eigenvectors directly from the null-space. 
-for i=1:6
-mat(:,:,i) = [(lambda1_d(i)*eye(6)-A1_app) B1_app];
+for i=1:length(lambda1_d)
+mat(:,:,i) = [(lambda1_d(i)*eye(length(lambda1_d))-A1_app) B1_app];
 nullspace(:,:,i) = null(mat(:,:,i),'r');
 vu(:,i) = 0.2*i*nullspace(:,1,i)+0.5*nullspace(:,2,i);
-V(:,i)=vu(1:6,i);
-U(:,i)=vu(7:8,i);
+V(:,i)=vu(1:length(lambda1_d),i);
+U(:,i)=vu(length(lambda1_d)+1:end,i);
 end
 
 K1 = U*inv(V); %returns K as complex variable but with 0 imaginary part
@@ -69,26 +72,28 @@ ti=0
 tf=del_t
 X=zeros(1,8) %final states will be stored here
 T=0          %final time will be stored here
-x0=[0 0 0 0 0 0 s_ref h_ref];
+x0=[0 0 0 0  s_ref h_ref 0 0];
 
-for i=1:n
-    
-    [t,x] = ode45('gsa_land_R',[ti tf],x0,[],A1_app,B1_app,K1,D1,u_ref);
-    
-    if x(end,8)>(x(end,7)*tan(gsa)+tol)
-        D1(end)=D1(end)-del_ws;
-    elseif x(end,8)<(x(end,7)*tan(gsa)-tol)
-        D1(end)=D1(end)+del_ws;
-    else
-        ;
-    end
-    
-    X=[X;x];
-    T=[T;t];
-    ti=tf
-    tf=tf+del_t;
-    x0=x(end,:);
-end
+ [t,x] = ode45('gsa_land',[ti tf],x0,[],A1_app,B1_app,K1,D1);
+
+% for i=1:n
+%     
+%    
+%     
+% %     if x(end,8)>(x(end,7)*tan(gsa)+tol)
+% %         D1(end)=D1(end)-del_ws;
+% %     elseif x(end,8)<(x(end,7)*tan(gsa)-tol)
+% %         D1(end)=D1(end)+del_ws;
+% %     else
+% %         ;
+% %     end
+%     
+%     X=[X;x];
+%     T=[T;t];
+%     ti=tf
+%     tf=tf+del_t;
+%     x0=x(end,:);
+% end
 
 
 %remember first row of X and T are zeros from initialization 
@@ -96,9 +101,9 @@ end
 figure(1)
 hold on
 %plot flight path,desired glide slope and runway start respectively
-plot(X(2:end,7),X(2:end,8))              
-plot(X(2:end,7),tan(gsa)*X(2:end,7),'r') 
-plot(X(2:end,7),zeros(size(X(2:end,7))),'k','LineWidth',2)
+plot(x(:,5),x(:,6))              
+plot(x(:,5),tan(gsa)*x(:,6),'r') 
+plot(x(:,5),zeros(size(x(:,6))),'k','LineWidth',2)
 plot(0,0,'.','MarkerSize',25)
 ylabel('h (ft)','FontSize',16),xlabel('s (ft)','FontSize',16)
 legend({'Flight path','Desired path','Runway','Runway start'},'FontSize',16,'Location','North')
